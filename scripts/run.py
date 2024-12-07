7638f4139d3c3639f582c5e467fe93c2ac8a0220
Maria Kesa <maria.kesa@gmail.com>
2024-12-07 05:33:32 +0200

    EID repository and Config#

--- scripts/run.py
+++ scripts/run.py
@@ -1,1 +1,7 @@
-from allen_vit_pipeline import pipeline
+from allen_vit_pipeline.pipeline import Config, EIDRepository
+
+config = Config('three_session_A', 'natural_movie_one')
+
+eids = EIDRepository(config).get_eids_to_process()
+print(eids)
+print(len(eids))
