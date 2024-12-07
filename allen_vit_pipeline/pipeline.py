773aa5f479761ef8c51de5a3bf2bea90a8137542
Maria Kesa <maria.kesa@gmail.com>
2024-12-07 02:30:41 +0200

    humble beginnings

--- allen_vit_pipeline/pipeline.py
+++ allen_vit_pipeline/pipeline.py
@@ -1,1 +1,15 @@
+from dotenv import load_dotenv
+import numpy as np
+import matplotlib.pyplot as plt
+from pathlib import Path
 from allensdk.core.brain_observatory_cache import BrainObservatoryCache
+import os
+import pickle
+
+load_dotenv()
+
+allen_cache_path = os.environ.get('HGMS_ALLEN_CACHE_PATH')
+save_path=os.environ.get('HGMS_STA_PATH')
+boc = BrainObservatoryCache(manifest_file=str(Path(allen_cache_path) / Path('brain_observatory_manifest.json')))
+
+#class ComputeSTA:
